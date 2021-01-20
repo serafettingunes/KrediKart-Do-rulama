d41e723a007f07226495723a4f66a11fa0dd04f6
serafettingunes <54938342+attila40x@users.noreply.github.com>
2021-01-20 17:43:50 +0300

    Add project files.

---
+++ KrediKartıDogrulama/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace KrediKartıDogrulama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int tek = 0, cift = 0, sayi = 0, k = 0;
+            long a = 8468335964683359;
+            int b = a.ToString().Length;
+            if (b == 16)
+            {
+                for (int i = 0; i <= 15; i++)
+                {
+                    sayi = Convert.ToInt32(a.ToString().Substring(i, 1));
+                    if (sayi % 2 == 0)
+                    {
+                        cift += sayi;
+                    }
+                    if (sayi % 2 == 1)
+                    {
+                        tek += (sayi * 2);
+                    }
+
+                }
+                if ((cift + tek) % 10 == 0 && (cift * 2) > 80)
+                {
+                    Console.WriteLine("doğru kredi kartı no girdin");
+                }
+                else
+                {
+                    Console.WriteLine(" kk no yanlış");
+                }
+
+
+
+            }
+
+        }
+    }
+}
